fd8dedbe35363254a11ae67aa5956713599a47eb
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-04-29 00:56:12 -0400

    add 0

--- append/append.js
+++ append/append.js
@@ -5,12 +5,13 @@ class append {
         const n = new Node(data);
         // check if size is 0
         if(this.size === 0){
-            // if size is 0 then add new node to header
+            // if size has 0 nodes then add new node to header
             this.header = n;
-            // if size is 0 then add new node to tail
+            // if size has 0 nodes then add new node to tail
             this.tail = n;
         }
         else{
+            // if size has node then 
             const temp = this.tail;
             this.tail = n
             temp.next = this.tail;
